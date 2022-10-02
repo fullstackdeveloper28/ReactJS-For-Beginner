44ac1759adfa13d0efb11531e3a56d2649f470c8
fullstackdeveloper28 <fullstackdeveloper28@gmail.com>
2022-10-02 11:20:35 +0530

    React for Beginners

---
+++ src/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Demo() {
+  return (
+   <div>
+     <h1>Full Stack {this.props.name}</h1>
+     <p>Welcome to my channel</p> 
+     </div>
+  )
+}
+
+export default Demo
